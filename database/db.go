87c27c60ba70c4aa252b3c4e888a34d9e79f586e
YC酱luyancib <2058282593@qq.com>
2025-06-02 16:12:31 +0800

    更新 db.go

--- database/db.go
+++ database/db.go
@@ -12,11 +12,11 @@ import (
 var Module = fx.Provide(NewDatabase)
 
 func NewDatabase(lc fx.Lifecycle, logger *zap.Logger) (*sql.DB, error) {
- user := os.Getenv("POSTGRES_USER")
- password := os.Getenv("POSTGRES_PASSWORD")
- host := os.Getenv("POSTGRES_HOST")
- port := os.Getenv("POSTGRES_PORT")
- dbname := os.Getenv("POSTGRES_DB")
+        user := os.Getenv("POSTGRES_USER")
+        password := os.Getenv("POSTGRES_PASSWORD")
+        host := os.Getenv("POSTGRES_HOST")
+        port := os.Getenv("POSTGRES_PORT")
+        dbname := os.Getenv("POSTGRES_DB")
 	connStr := "postgresql://postgres:114514@localhost:5432/postgres?sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
