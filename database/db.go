cf9a63f24b4b0fa7a4c5be8ffef1b6a5caf4b3c1
YC酱luyancib <2058282593@qq.com>
2025-06-02 16:11:18 +0800

    更新 db.go

--- database/db.go
+++ database/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	_ "github.com/lib/pq"
+ "os"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -11,6 +12,11 @@ import (
 var Module = fx.Provide(NewDatabase)
 
 func NewDatabase(lc fx.Lifecycle, logger *zap.Logger) (*sql.DB, error) {
+ user := os.Getenv("POSTGRES_USER")
+ password := os.Getenv("POSTGRES_PASSWORD")
+ host := os.Getenv("POSTGRES_HOST")
+ port := os.Getenv("POSTGRES_PORT")
+ dbname := os.Getenv("POSTGRES_DB")
 	connStr := "postgresql://postgres:114514@localhost:5432/postgres?sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
